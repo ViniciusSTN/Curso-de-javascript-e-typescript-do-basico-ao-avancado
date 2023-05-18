9b3a2547a6cf342bba89f040e43b182c8e361d9a
Vinicius Santana <124202530+ViniciusSTN@users.noreply.github.com>
2023-05-17 21:22:18 -0300

    aula 128

--- secao10 - JS - Node - Npm - Express e MongoDB - NodeJS/128 - listando arquivos com FS e recursao mutua/index.js
+++ secao10 - JS - Node - Npm - Express e MongoDB - NodeJS/128 - listando arquivos com FS e recursao mutua/index.js
@@ -1,7 +1,5 @@
-const fs = require('fs').promises;
-const path = require("path");
-
-// fs == file system
+const fs = require('fs').promises;  // fs == file system
+const path = require("path");       // path == caminho
 
 // fs.readdir('./')
 //     .then(files => console.log(files))
@@ -10,13 +8,33 @@ const path = require("path");
 async function readdir(rootDir) {
     rootDir = rootDir || path.resolve(__dirname);
     const files = await fs.readdir(rootDir);
-    walk(files);
+    walk(files, rootDir);
 }
 
-function walk(files) {
+async function walk(files, rootDir) {
     for (const file of files) {
+        // pegar o caminho do diretório atual e atribuir o file nesse caminho
+        const fileFullPath = path.resolve(rootDir, file); 
+        // console.log(fileFullPath);
+        const stats = await fs.stat(fileFullPath);
+
+        // desconsiderar arquivos da pasta \.git e \.node_modules
+        if (/\.git/g.test(fileFullPath)) continue;
+        if (/\.node_modules/g.test(fileFullPath)) continue;
+
+        // se for pasta, ler arquivos da pasta
+        if (stats.isDirectory()) {
+            readdir(fileFullPath);
+            continue;
+        }
+
+        // para listar apenas CSS
+        // if (!/\.css$/g.test(fileFullPath)) continue;
+
         console.log(file);
     }
 }
 
-readdir();
+readdir('/Users/vinic/Desktop/Cursos/Curso de javascript e typescript do basico ao avancado');
+
+// /Users/vinic/Desktop/Cursos/Curso de javascript e typescript do basico ao avancado/secao10 - JS - Node - Npm - Express e MongoDB - NodeJS
