c34258ecf7c51a0808ebe93cdcabf246b0973779
Vinicius Santana <124202530+ViniciusSTN@users.noreply.github.com>
2023-05-24 18:19:03 -0300

    aula145

---
+++ secao10 - JS - Node - Npm - Express e MongoDB - NodeJS/145-Helmet_e_CSRF/src/middlewares/middleware.js
@@ -0,0 +1,24 @@
+exports.middlewareGlobal = (req, res, next) => {
+  res.locals.umaVariavelLocal = 'Este é o valor da variável local.';
+  next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+  next();
+};
+
+// -----------------------------------------------------------
+// aula atual
+
+exports.checkCsrfError = (err, req, res, next) => {
+  if(err && 'EBADCSRFTOKEN' === err.code) {
+    return res.render('404');
+  }
+};
+
+exports.csrfMiddleware = (req, res, next) => {
+  res.locals.csrfToken = req.csrfToken(); // cria token
+  next();
+};
+
+// -----------------------------------------------------------
