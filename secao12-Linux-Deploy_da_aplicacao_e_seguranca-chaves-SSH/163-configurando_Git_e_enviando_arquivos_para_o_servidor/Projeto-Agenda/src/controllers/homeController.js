27e353928eaef28a7fffd7e188b6dcb5fdfb4812
ViniciusSTN <vinicius.stn@gmail.com>
2023-05-30 18:28:02 -0300

    aula164

--- secao12-Linux-Deploy_da_aplicacao_e_seguranca-chaves-SSH/163-configurando_Git_e_enviando_arquivos_para_o_servidor/Projeto-Agenda/src/controllers/homeController.js
+++
@@ -1,9 +0,0 @@
-const Contact = require('../models/ContactModel');
-
-exports.index = async (req, res) => {
-  const contacts = await Contact.findContacts();  // buscando contatos salvos
-  res.render('index', { contacts });
-  return;
-};
-
-
