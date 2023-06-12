87589cfa977cf94045d2a4c8734d5cb9707b2fb4
ViniciusSTN <vinicius.stn@gmail.com>
2023-06-12 19:32:43 -0300

    aula184

--- secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/controllers/FotoController.js
+++ secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+// single pois é um unico arquivo, e seu parâmetro é o nome do campo onde vai receber o arquivo
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file); // dados do arquivo enviado
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code], // exibe a mensagem de erro definida no arquivo multerConfig
+        });
+      }
+
+      return res.json(req.file); // dados do arquivo enviado
+    });
   }
 }
 
--- secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/routes/fotoRoutes.js
+++ secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/routes/fotoRoutes.js
@@ -1,14 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-// single pois é um unico arquivo, e seu parâmetro é o nome do campo onde vai receber o arquivo
-router.post('/', upload.single('foto'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
