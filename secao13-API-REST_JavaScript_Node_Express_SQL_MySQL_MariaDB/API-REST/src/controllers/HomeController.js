f18124b4d7c1a133f5987799ec8fc7c97c2c0ff8
ViniciusSTN <vinicius.stn@gmail.com>
2023-06-12 17:08:06 -0300

    aula181

--- secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/controllers/HomeController.js
+++ secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Ilza',
-      sobrenome: 'Santana',
-      email: 'email2@gmail.com',
-      idade: 55,
-      peso: 85,
-      altura: 1.6,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
--- secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/routes/userRoutes.js
+++ secao13-API-REST_JavaScript_Node_Express_SQL_MySQL_MariaDB/API-REST/src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 // não deveria existir em uma API real
-router.get('/', loginRequired, userController.index);
+// router.get('/', loginRequired, userController.index);
 // router.get('/:id', userController.show);
 
 router.post('/', userController.store);
