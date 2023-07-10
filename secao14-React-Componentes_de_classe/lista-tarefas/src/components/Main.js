c8124096adc49011753cf36e4812cafbd78d8fc2
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-10 16:12:43 -0300

    aula198

--- secao14-React-Componentes_de_classe/lista-tarefas/src/components/Main.js
+++ secao14-React-Componentes_de_classe/lista-tarefas/src/components/Main.js
@@ -2,14 +2,33 @@ import React, { Component } from "react";
 
 import { FaPlus } from 'react-icons/fa';
 
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
 import './Main.css';
 
 export default class Main extends Component {
 
   // hoje é possivel inicializar o state sem o constructor, por conta do class fields
   state = {
-    novaTarefa: '', // inicializa state
+    novaTarefa: '',
+    tarefas: []
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {tarefas} = this.state;
+    let {novaTarefa} = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    // se a tarefa já existir
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    })
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -17,15 +36,29 @@ export default class Main extends Component {
     })
   }
 
+  handleEdit = (e, index) => {
+    console.log('Edit', index);
+  }
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    })
+  }
+
   render() {
-    const {novaTarefa} = this.state;
+    const {novaTarefa, tarefas} = this.state;
 
     return (
       // className refere-se a class do html
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           <input
             onChange={this.handleChange}
             type="text"
@@ -35,6 +68,26 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            // essa key é para o evitar erro no react (tirar e ver o erro no navegador)
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit
+                  className="edit"
+                  onClick={(e) => this.handleEdit(e, index)}
+                />
+
+                <FaWindowClose
+                  className="delete"
+                  onClick={(e) => this.handleDelete(e, index)}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
