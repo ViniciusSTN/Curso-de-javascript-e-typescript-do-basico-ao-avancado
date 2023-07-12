da9b2757c69314a632d534dd92a86196777fd7d4
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-12 11:52:23 -0300

    aula207

---
+++ secao15-React-Redux-ReduxSaga-Hooks-ComponentesFuncionais/react-base/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+
+import { Nav } from './styled';
+
+export default function Header() {
+  return (
+    <Nav>
+      <a href="https://google.com">
+        <FaHome size={24} />
+      </a>
+      <a href="https://google.com">
+        <FaUserAlt size={24} />
+      </a>
+      <a href="https://google.com">
+        <FaSignInAlt size={24} />
+      </a>
+    </Nav>
+  );
+}
