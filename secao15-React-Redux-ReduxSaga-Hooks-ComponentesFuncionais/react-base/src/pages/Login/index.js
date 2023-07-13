379b44faeaf8b76d705fe6d94d57a2ce6cda6c45
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-13 10:58:40 -0300

    aula214

--- secao15-React-Redux-ReduxSaga-Hooks-ComponentesFuncionais/react-base/src/pages/Login/index.js
+++ secao15-React-Redux-ReduxSaga-Hooks-ComponentesFuncionais/react-base/src/pages/Login/index.js
@@ -3,7 +3,20 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  // substitui os estados de componentes usados em componentes de classe
+  // primeiro parâmetro é a função, o segundo é um array de dependências para executar mais de uma vez a função caso necessário
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/alunos');
+      const { data } = response;
+      console.log(data);
+    }
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title isRed={false}>
