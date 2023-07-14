71bd21aa1492a9462cc16c28623fc0bf98c779da
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-13 22:35:48 -0300

    aula217

--- secao15-React-Redux-ReduxSaga-Hooks-ComponentesFuncionais/react-base/src/store/index.js
+++ secao15-React-Redux-ReduxSaga-Hooks-ComponentesFuncionais/react-base/src/store/index.js
@@ -1,7 +1,13 @@
-import { legacy_createStore } from 'redux';
+import { legacy_createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
 
-const store = legacy_createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = legacy_createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
