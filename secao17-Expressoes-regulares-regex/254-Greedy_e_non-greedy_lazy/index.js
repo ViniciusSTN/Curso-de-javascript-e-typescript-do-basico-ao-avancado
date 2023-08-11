2b0f9f63b4b1e9d7d339760b435bafbeb1dc09a0
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-11 20:37:45 -0300

    aula254

---
+++ secao17-Expressoes-regulares-regex/254-Greedy_e_non-greedy_lazy/index.js
@@ -0,0 +1,19 @@
+const html = '<h1>olá</h1>  <h2>olá novamente</h2>';
+
+// . -> qualquer caractere
+
+// selecionar: <*>*</*>
+
+// greedy:
+const regExp1 = /<.+>.+<\/.+>/g;
+const found1 = html.match(regExp1); // [ '<h1>olá</h1>  <h2>olá novamente</h2>' ]
+// nesse caso tem um comportamento greedy (ambicioso), pois não separa os conteudos
+
+
+
+// non-greedy:
+const regExp2 = /<.+?>.+?<\/.+?>/g;
+const found2 = html.match(regExp2); // [ '<h1>olá</h1>', '<h2>olá novamente</h2>' ]
+
+console.log(found1);
+console.log(found2);
