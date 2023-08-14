81efa0a872eba9ee6c6b890410524b94269597a1
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-14 19:52:27 -0300

    aula295

---
+++ secao20-Classes-e-interfaces-POO/src/295-Modificadores-de-acesso/index.ts
@@ -0,0 +1,16 @@
+// (por padrão é public)
+
+// public -> acessivel na classe, fora e subclasses
+// private -> acessivel dentro da classe
+
+export class Empresa {
+  public readonly nome: string;
+
+  constructor(nome: string) {
+    this.nome = nome;
+  }
+}
+
+const empresa1 = new Empresa('Udemy');
+
+console.log(empresa1);
