41197eb53349edd956cecc9e54e9fc4f38e1a154
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-15 18:56:01 -0300

    aula304

---
+++ secao20-Classes-e-interfaces-POO/src/304-Inversao-de-dependencia/index.ts
@@ -0,0 +1,4 @@
+// o que foi feito no ultimo código foi inversão de dependência
+// classes abstratas e interfaces permitem isso
+
+// no caso do ultimo código daria para apontar a ferramenta diretamente para a Caneta ou MaquinaEscrever, mas é mais seguro apontar para a Ferramenta, pois caso a caneta deixar de existir não quebra o código
