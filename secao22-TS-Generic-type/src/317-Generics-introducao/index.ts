4e678bae6fed4a62df0b45d0ab11774a8831a6ca
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-17 17:32:10 -0300

    aula317

---
+++ secao22-TS-Generic-type/src/317-Generics-introducao/index.ts
@@ -0,0 +1,25 @@
+// o T de meuFilter<T> é passado para FilterCallback<U>, ou seja, o tipo T é igual a U
+// como se fosse parâmetros e argumentos
+type FilterCallback<U> = (value: U, index?: number, array?: U[]) => boolean;
+
+// para atribuir tipo genérico <T> (na verdade pode ser qualquer coisa dentro de <>, mas por convenção usa-se T)
+// na hora da chamada da função o tipo vai ser inferido e passado para os outros T
+export function meuFilter<T>(array: T[], callbackfn: FilterCallback<T>): T[] {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrayFiltrado = meuFilter(array, (value) => value < 5);
+console.log(arrayFiltrado);
+
+const array2 = ['a', 'a', 'b', 'c'];
+const arrayFiltrado2 = meuFilter(array2, (value) => value !== 'a');
+console.log(arrayFiltrado2);
