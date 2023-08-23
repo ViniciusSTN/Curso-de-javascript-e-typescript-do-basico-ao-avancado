31d89d266d096d1f8f0af8af6d8b3b68532698a8
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-23 15:11:27 -0300

    aula356

--- secao26-Introducao-ao-Jest/src/services/persistency.spec.ts
+++ secao26-Introducao-ao-Jest/src/services/persistency.spec.ts
@@ -1,11 +1,26 @@
-describe('TESTANDO ALGUMA COISA', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
+import { Persistency } from './persistency';
+
+describe('Persistency', () => {
+  // limpa os mocks depois de cada teste -> para não dar erro nos dois ultimos testes
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // System under test
+    const sut = new Persistency(); // sut -> classe que está sendo testada
+    expect(sut.saveOrder()).toBeUndefined();
   });
 
-  test('shuld return Luiz', () => {
-    const nome = 'Luiz';
-    expect(nome).toBe('Luiz');
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
