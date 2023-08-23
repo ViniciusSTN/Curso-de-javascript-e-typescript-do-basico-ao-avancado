7a83947c8331a8422b72df7998c101a5b280fbce
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-23 17:25:49 -0300

    aula362

--- secao26-Introducao-ao-Jest/src/classes/shopping-cart.spec.ts
+++ secao26-Introducao-ao-Jest/src/classes/shopping-cart.spec.ts
@@ -71,13 +71,14 @@ describe('ShoppingCart', () => {
     const { sut, discountMock } = createSutWithProducts();
     const discountMockSpy = jest.spyOn(discountMock, 'calculate');
     sut.totalWithDiscount();
-    expect(discountMockSpy).toHaveBeenCalledTimes(1);
+    expect(discountMockSpy).toHaveBeenCalledTimes(1); // quando totalWithDiscount for chamado, discountMockSpy deve ser executado uma vez, ou seja, a função calculate deve ser chamada
   });
 
+  // teste de integração
   it('should call discount.calculate with total price when totalWithDiscount is called', () => {
     const { sut, discountMock } = createSutWithProducts();
     const discountMockSpy = jest.spyOn(discountMock, 'calculate');
     sut.totalWithDiscount();
-    expect(discountMockSpy).toHaveBeenCalledWith(sut.total());
+    expect(discountMockSpy).toHaveBeenCalledWith(sut.total()); // se calculate vai ser chamado com o método sut.total()
   });
 });
