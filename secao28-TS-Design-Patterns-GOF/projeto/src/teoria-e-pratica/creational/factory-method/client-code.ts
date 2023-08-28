c01f26f100f6980fc31a855c4f7eb8997c9e73aa
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-28 16:30:52 -0300

    aula390

--- secao28-TS-Design-Patterns-GOF/projeto/src/teoria-e-pratica/creational/factory-method/client-code.ts
+++ secao28-TS-Design-Patterns-GOF/projeto/src/teoria-e-pratica/creational/factory-method/client-code.ts
@@ -2,7 +2,13 @@ import { randomCarAlgorithm } from './main/random-vehicle-algorithm';
 import { randomNumbers } from './utils/random-numbers';
 import { CarFactory } from './factories/car-factory';
 
+// desacopla o código cliente das classes concretas
+// as factories ficam acopladas às classes concretas (único ponto que fica acoplado)
+
+// obs: código cliente são os códigos usados em várias partes do projeto
+
 const carFactory = new CarFactory();
+
 const customerNames = ['Ana', 'Joana', 'Helena', 'João'];
 
 for (let i = 0; i < 10; i++) {
