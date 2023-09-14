dab409128e8d04edd73f582ed66f67f1163a6f6d
ViniciusSTN <vinicius.stn@gmail.com>
2023-09-14 19:05:08 -0300

    aula459

--- secao29-NextJS_e_strapi/blog-next/src/containers/Post/index.tsx
+++ secao29-NextJS_e_strapi/blog-next/src/containers/Post/index.tsx
@@ -2,6 +2,7 @@ import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import { Heading } from '@/components/Heading';
 import { MainContainer } from '@/components/MainContainer';
+import { PostCover } from '@/components/PostCover';
 import { PostData } from '@/domain/posts/posts';
 
 export type PostProps = {
@@ -15,6 +16,10 @@ export const Post = ({ post }: PostProps) => {
 
       <MainContainer>
         <Heading>{post.attributes.title}</Heading>
+        <PostCover
+          coverUrl={post.attributes.cover.data.attributes.formats.medium.url}
+          alt={post.attributes.title}
+        />
         <div>{post.attributes.content}</div>
       </MainContainer>
 
