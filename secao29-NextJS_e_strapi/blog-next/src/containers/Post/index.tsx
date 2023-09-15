e1986fd63d7b659cdb1b2ff5b593178fd1b3c61e
ViniciusSTN <vinicius.stn@gmail.com>
2023-09-15 11:52:57 -0300

    aula466

--- secao29-NextJS_e_strapi/blog-next/src/containers/Post/index.tsx
+++ secao29-NextJS_e_strapi/blog-next/src/containers/Post/index.tsx
@@ -6,17 +6,28 @@ import { MainContainer } from '@/components/MainContainer';
 import { PostContainer } from '@/components/PostContainer';
 import { PostCover } from '@/components/PostCover';
 import { PostDetails } from '@/components/PostDetails';
+import { SITE_NAME } from '@/config/app-config';
 import { PostData } from '@/domain/posts/posts';
+import { removeHtml } from '@/utils/remove-html';
+import Head from 'next/head';
 
 export type PostProps = {
   post: PostData;
 };
 
 export const Post = ({ post }: PostProps) => {
-  console.log('DATAAA' + post.attributes.createdAt);
-
   return (
     <>
+      <Head>
+        <title>
+          {post.attributes.title} - {SITE_NAME}
+        </title>
+        <meta
+          name="description"
+          content={removeHtml(post.attributes.content).slice(0, 150)}
+        />
+      </Head>
+
       <Header />
 
       <MainContainer>
